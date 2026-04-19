66da681de37ea2c4c2b688713613cf0a7818061a
Arif Akhtar <ansariarif121124421@gmail.com>
2026-04-19 17:46:32 +0530

    changes 2

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import HeroSection from "../app/sections/HeroSection";
 import AboutSection from "../app/sections/AboutSection";
 import ProductSearch from "../app/sections/ProductSearch";
-import GroceriesPreview from "../app/sections/GroceriesPreview";
 import PYQSection from "../app/sections/Pyqsection";
 import ServicesPreview from "../app/sections/ServicePreview";
 import StationeryPreview from "../app/sections/StationeryPreview";
@@ -13,7 +12,6 @@ export default function Home() {
       <ServicesPreview />
       {/* <ProductSearch /> */}
       <StationeryPreview />
-      <GroceriesPreview />
       <PYQSection />
       {/* <AboutSection /> */}
     </main>
